6f194cb9402967f83ed463ca01c6134bdc03d21d
biwasbhandari <biwas2059@gmail.com>
2025-05-07 00:16:45 +0545

    feat: dynamic navigation

---
+++ Landmarks/Views/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Biwas Bhandari on 06/05/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+#Preview {
+    ContentView()
+}
